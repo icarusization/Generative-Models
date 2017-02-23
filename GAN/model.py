b6167c5c84ccc4e60314842e491b72eb64ead119
Fantasy_fish <shyay1013@sjtu.edu.cn>
2017-02-23 08:12:33 +0800

    change the loss

--- GAN/model.py
+++ GAN/model.py
@@ -99,15 +99,14 @@ class DCGAN(object):
         self.d__sum = histogram_summary("d_", self.D_)
         self.G_sum = image_summary("G", self.G)
 
-        self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
-        self.d_loss_fake = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
-        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+        self.d_loss_real = tf.reduce_mean(self.D_logits)
+        self.d_loss_fake = tf.reduce_mean(self.D_logits_)
+        self.g_loss = -self.d_loss_fake
 
         self.d_loss_real_sum = scalar_summary("d_loss_real", self.d_loss_real)
         self.d_loss_fake_sum = scalar_summary("d_loss_fake", self.d_loss_fake)
 
-        self.d_loss = self.d_loss_real + self.d_loss_fake
+        self.d_loss = -self.d_loss_real + self.d_loss_fake
 
         self.g_loss_sum = scalar_summary("g_loss", self.g_loss)
         self.d_loss_sum = scalar_summary("d_loss", self.d_loss)
