e3e05ecdff96f7aea4bbdd731a1c65ae6ca15127
fantasyfish <shyay1013@gmail.com>
2017-04-20 20:32:49 +0800

    add Pre-training and Condition Augmentation options

--- StackGAN/StageI/main.py
+++ StackGAN/StageI/main.py
@@ -11,9 +11,10 @@ import tensorflow as tf
 
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 600, "Epoch to train [25]")
-flags.DEFINE_float("learning_rate_d", 0.0002, "Learning rate of for rmsprop [0.0002]")
-flags.DEFINE_float("learning_rate_g", 0.0002, "Learning rate of for rmsprop [0.0002]")
+flags.DEFINE_float("learning_rate_d", 0.0001, "Learning rate of for rmsprop [0.0002]")
+flags.DEFINE_float("learning_rate_g", 0.0001, "Learning rate of for rmsprop [0.0002]")
 flags.DEFINE_float("lr_decay_step", 100, "number of epochs when learning rate is decayed by 1/2")
+flags.DEFINE_float("is_CA",False,"True for caption augmentation")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
@@ -28,6 +29,7 @@ flags.DEFINE_boolean("is_crop", False, "True for training, False for testing [Fa
 #flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 #flags.DEFINE_boolean("is_GUI", False, "True for GUI, False for nothing [True]")
 flags.DEFINE_integer("Lambda", 10, "Gradient penalty lambda hyperparameter")
+flags.DEFINE_integer("d_pre_train_step",100,"Number of steps for pre-training the discriminator")
 flags.DEFINE_integer("d_iters",5, "Number of discriminator training steps per generator training step")
 flags.DEFINE_integer("g_iters",1, "Number of generator training steps per generator training step")
 flags.DEFINE_integer("y_dim",128,"Number of dimensions for y")
@@ -57,9 +59,10 @@ def main(_):
                       Lambda=FLAGS.Lambda,
                       dataset_name=FLAGS.dataset,
                       is_crop=FLAGS.is_crop,
+                      is_CA=FLAGS.is_CA,
                       checkpoint_dir=FLAGS.checkpoint_dir,
                       sample_dir=FLAGS.sample_dir,
-                      model_name='dcgan0')
+                      model_name='stackgan/stage1')
 
         if FLAGS.is_train:
             stackgan.train(FLAGS)
